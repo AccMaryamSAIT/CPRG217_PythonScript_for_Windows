7c202b818bb86b42601197f6c4da7a745b6a1655
AccMaryamSAIT <130182503+AccMaryamSAIT@users.noreply.github.com>
2023-07-03 07:57:41 -0600

    ver1.2

--- Script1-bedtimesetter.py
+++ Script1-bedtimesetter.py
@@ -1,39 +1,259 @@
 '''
 Name: Script1-bedtimesetter.py
 Authors: Roman Kapitoulski, Eric Russon, Maryam Bunama
-Version: 0.0
-Date: June 29, 2023
+Version: 1.2
+Date: July 3, 2023
 Description: This script has been made for a parent to set an alarm for children when their bedtime approaches. 
-It has the options to set the timer, view the time set, veto the timer (for a period of time), save and exit, 
-or save without exiting.
+It has the options to set the timer, view the time set, veto the timer (until a set time limit), save and exit, 
+or save without exiting. The time format used corresponds to 'hh:mm AM/PM' throughout. The code won't accept vetoing the time 
+if no time was previously set.
 '''
-option = 0
-while option != 5:
-    print('\nWelcome to Bedtime Alarm Setter!')
-    print('Select from the following options:')
-    print('1- Set timer')
-    print('2- View configurations')
-    print('3- Veto timer')
-    print('4- Save and exit')
-    print('5- Exit without saving')
+
+import win32com.client as win32 # Module to create and edit excel file in this script
+from tabulate import tabulate # Module that displays data in a table
+import getpass, os, datetime 
+
+## FUNCTIONS
+def getPassword():
+    # Prompt user to enter password
+    password = getpass.getpass('Enter password: ')
+    return password
+
+def authenticate(password):
+    # Preset password to compare against inputted password
+    return password == '1'
+
+def formatDecimalTime(decimalTime):
+    # Convert decimal time to hours and minutes
+    total_minutes = decimalTime * 24 * 60  # Convert to total minutes in a day
+    hours = int(total_minutes // 60)  # Extract the whole number of hours
+    minutes = int(total_minutes % 60)  # Extract the remaining minutes
+
+    # Create a time object with the converted hours and minutes
+    timeObj = datetime.time(hours, minutes)
+
+    # Format the time object as %I:%M %p
+    formattedTime = timeObj.strftime('%I:%M %p')
     
-    option = int(input('>>> '))
-    if option == 1:
-        # code to set the timer
-        print('\nTimer set')
-    elif option == 2:
-        # code to view timer and configurations
-        timer = 0 # TEMPORARY VALUE
-        print(f'\nTimer set to: {timer}')
-    elif option == 3:
-        # code to veto
-        print('\nTimer vetoed')
-    elif option == 4:
-        # code to write changes to excel file
-        print('\nSaved and exiting program...\n')
-        exit()
-    elif option == 5:
-        print('\nExiting program...\n')
-        exit()
+    return formattedTime
+
+def createExcelFile():
+    # Dispatch excel file and create a workbook
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = excel.Workbooks.Add()
+    
+    # Name the worksheet
+    ws1 = wb.Worksheets(1)
+    ws1.Name = 'Settings'
+
+    # Assigning static values to header cells
+    headerValues = {
+    'A1': 'Time Limit',
+    'B1': 'Veto Status',
+    'C1': 'Veto Until',
+    'D1': 'Date set',
+    'E1': 'Parent Email' }
+
+    for headerAddress, headerValue in headerValues.items():
+        ws1.Range(headerAddress).Value = headerValue
+
+    # Save with a preset filename
+    wb.SaveAs(os.path.join(f'{os.getcwd()}/alarmsettings.xlsx'))
+    wb.Close()    
+
+def dispatchExcelFile(filepath):
+    # Dispatch the file and return the excel file and worksheet variables
+    excelApp = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = excelApp.Workbooks.Open(filepath)
+    ws1 = wb.Worksheets(1)
+    return ws1
+
+def setEmail(filepath):
+    # Dispatch the file
+    ws1 = dispatchExcelFile(filepath)
+
+    # Input prompt
+    print('\nRemember to save changes after setting your email.')
+    email = input('Please input your email\n>>> ')
+
+    # Adding input to excel file
+    cellAddress = 'E2'
+    cellValue = email
+    ws1.Range(cellAddress).Value = cellValue
+
+def setTime(filepath):
+    # Dispatch excel file
+    ws1 = dispatchExcelFile(filepath)
+
+    # Input prompt
+    print('\nInput time in the "hh:mm AM/PM" format.')
+    timeLimit = input('>>> ')
+
+    # Format time and print error if user inputs the wrong information
+    try:
+        timeObj = datetime.datetime.strptime(timeLimit, '%I:%M %p')
+        timeFormatted = timeObj.strftime('%I:%M %p')
+    except ValueError:
+        print('Invalid time format. Please use the "hh:mm AM/PM" format.')
+        return
+
+    # Update cell values
+    cellValues = {
+    'A2': timeFormatted,
+    'B2': False,
+    'C2': None,
+    'D2': datetime.datetime.now().strftime('%b/%d/%Y %I:%M %p')
+    }
+
+    # Add the data to the specified cells in excel
+    for cell in cellValues.items():
+        cellAddress = cell[0]
+        cellValue = cell[1] 
+        ws1.Range(cellAddress).Value = cellValue
+    
+    print('\nSetting time...')
+
+def setVetoTime(filepath):
+    # Dispatch excel file
+    ws1 = dispatchExcelFile(filepath)
+
+    # Print error if no time was previously set
+    timeCell = ws1.Range('A2').Value
+    if timeCell is None:
+        print('\nOperation can\'t be performed. Please set the time limit first (Option: 1).')
+        return
+
+    print('\nInput veto time in the "hh:mm AM/PM" format')
+    timeVeto = input('>>> ')
+
+    # Format time and print error if user inputs the wrong information
+    try:
+        timeObj = datetime.datetime.strptime(timeVeto, '%I:%M %p')
+        timeFormatted = timeObj.strftime('%I:%M %p')
+    except ValueError:
+        print('Invalid time format. Please use the "hh:mm AM/PM" format.')
+        return
+
+    # Update cell values
+    cellValues = {
+    'B2': True,
+    'C2': timeFormatted,
+    'D2': datetime.datetime.now().strftime('%M/%d/%Y %I:%M %p')
+    }
+
+    # Add the data to the specified cells in excel
+    for cell in cellValues.items():
+        cellAddress = cell[0]
+        cellValue = cell[1] 
+        ws1.Range(cellAddress).Value = cellValue
+    
+    print('\nVetoing time...')
+
+def viewTimeSettings(filepath):   
+    # Dispatch excel file
+    ws1 = dispatchExcelFile(filepath)
+
+    # Get the used range of the worksheet and convert it into a formatted Excel table
+    usedRange = ws1.UsedRange
+    tableData = usedRange.Value
+
+    # Convert the table data to a list of rows
+    rows = [list(row) for row in tableData]
+    
+    # Extract the header row and remove it from the rows list
+    headers = rows.pop(0)
+
+    # Only format the specified cells if time was inputted. 
+    timeCell = ws1.Range('A2').Value
+    if timeCell is None:
+        pass
     else:
-        print('\nInvalid input. Please input the correct values.')
+        # Format the time cell for time limit
+        timeSetCell = rows[0][0]
+        formattedTimeSetCell = formatDecimalTime(timeSetCell)
+        rows[0][0] = formattedTimeSetCell
+
+    timeCell = ws1.Range('C2').Value
+    if timeCell is None:
+       pass
+    else:
+        # Format the time cell for time veto 
+        timeVetoCell = rows[0][2]
+        formattedTimeVetoCell = formatDecimalTime(timeVetoCell)
+        rows[0][2] = formattedTimeVetoCell
+
+
+    # Display the table in the terminal
+    print(tabulate(rows, headers=headers, tablefmt='fancy_grid'))
+
+def saveExcelFile(filepath):
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = excel.Workbooks.Open(filepath)
+    wb.Close(SaveChanges=True) # Writes changes to excel file
+    excel.Quit()
+
+def exitExcelFileWithoutSaving(filepath):
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = excel.Workbooks.Open(filepath)
+    wb.Close(SaveChanges=False) # Ensures that no changes are written to excel file
+    excel.Quit()
+
+def menu():
+    option = 0
+    while option != 5:
+        # Takes the filepath of the excel settings file
+        filepath = f'{os.getcwd()}/alarmsettings.xlsx'
+
+        # Menu printed out with list of options. Each option corresponds to a number
+        print('\nWelcome to Bedtime Alarm Setter!')
+        print('Select from the following options:')
+        print('1- Set timer')
+        print('2- View configurations')
+        print('3- Veto timer')
+        print('4- Set Email')
+        print('5- Save and exit')
+        print('6- Exit without saving')
+        
+        option = int(input('>>> '))
+        if option == 1:
+            setTime(filepath) # Function for setting the time. 
+        elif option == 2:
+            viewTimeSettings(filepath) # Function to display table of time settings
+        elif option == 3:    
+            setVetoTime(filepath) # Function for setting time to veto
+        elif option == 4:       
+            setEmail(filepath) # Function for setting the email
+        elif option == 5:
+            saveExcelFile(filepath) # Save excel file and exit
+            print('\nSaved and exiting program...\n')
+            exit()
+        elif option == 6:
+            exitExcelFileWithoutSaving(filepath) # Exiting program without saving changes
+            print('\nExiting program...\n')
+            exit()
+        else:
+            print('\nInvalid input. Please input the correct values.')
+
+def main():
+    # Takes the filepath of the excel settings file
+    filepath = f'{os.getcwd()}/alarmsettings.xlsx'
+
+    # Create a new file if program file doesn't exist or was deleted
+    if os.path.isfile(filepath):
+        pass
+    else:  
+        createExcelFile()
+
+    # Assigns inputted password to a variable
+    password = getPassword()
+
+    # Authenticates password against the preset password
+    if authenticate(password):  
+        # Launches menu if authentication successful
+        print('Authentication successful!')
+        menu()
+    else:
+        print('Authentication failed. Access denied.')
+
+# Launch the program
+main()
---
+++ Script2-bedtimealarm.py
@@ -0,0 +1,121 @@
+'''
+Name: bedtimealarm.py
+Authors: Roman Kapitoulski, Eric Russon, Maryam Bunama
+Version: 1.2
+Date: July 3, 2023
+Description: This script has been made to run in the background and monitoring a kid's PC hours. Once he reaches the time limit,
+he will see a pop-up dialog box with two options. If he chooses 'Yes', the computer will shut down after 5 mintues. If he chooses 'no',
+he will be able to continue using the computer but an email will be sent to the parent. 
+'''
+
+import winsound # Module to play a Windows Alarm
+import threading # Module that allows alarm to start at the same time as the dialog box
+import win32com.client as win32 # Module to access excel configuration file and send email
+import win32api # Module to use the popup dialog box
+import win32gui # Module to display the popup dialog box
+import win32con # Module that contains the constants for the dialog box options
+import os, time, datetime, schedule
+
+## EXCEPTION
+# Built in exception to stop the scheduler if conditions are met
+class DialogFinished(Exception):
+    pass
+
+## FUNCTIONS 
+def playWindowsAlarm():
+    # Set alarm name based on Registry editor key naming.
+    alarmName = "Notification.Looping.Alarm"
+
+    # Play alarm 
+    winsound.PlaySound(alarmName, winsound.SND_ALIAS)
+
+def formatDecimalTime(decimalTime):
+    # Convert decimal time to hours and minutes
+    hours = int(decimalTime * 24)
+    minutes = int((decimalTime * 24 * 60) % 60)
+
+    # Create a time object with the converted hours and minutes
+    timeObj = datetime.time(hours, minutes)
+
+    # Format the time object as HH:MM
+    formattedTime = timeObj.strftime("%H:%M")
+    return formattedTime
+
+def dispatchExcelFile(filepath):
+    # Dispatch the file and return the excel file and worksheet variables
+    excelApp = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = excelApp.Workbooks.Open(filepath)
+    ws1 = wb.Worksheets(1)
+    return ws1
+
+def Emailer(parentEmail):
+    # Dispatch outlook
+    outlook = win32.Dispatch('outlook.application')
+
+    # Create an email
+    mail = outlook.CreateItem(0)
+
+    # Setting email recipient, subject, and body
+    mail.To = parentEmail
+    mail.Subject = 'Child chose to ignore the alarm'
+    mail.Body = f'You are recieveing this email because your child chose to ignore the alarm you previously set.'
+
+    # Sends email to reciepient
+    mail.Send()
+
+def displayDialog(email):
+
+    # Define the dialog box properties
+    title = "Time is Up!"
+    message = "You reached your PC time limit. Do you want to shut down?"
+
+    # Create a thread for playing the alarm sound. This allows the alarm and the dialog box to play at the same time
+    alarmThread = threading.Thread(target=playWindowsAlarm)
+    alarmThread.start() # Start the alarm sound thread
+    
+    # Create the dialog box
+    response = win32api.MessageBox(win32gui.GetForegroundWindow(), message, title, win32con.MB_ICONWARNING | win32con.MB_YESNO)
+    alarmThread.join() # Join the alarm sound thread to wait for it to finish
+
+    # Handle the user's response
+    if response == win32con.IDYES: # User clicks 'Yes'
+        print("Shutting down the computer in 5 minutes")
+        # time.sleep (5) # FOR DEMO PURPOSES - wait for 5 seconds
+        time.sleep(5 * 60) # Wait for 5 minutes
+        os.system("shutdown /s /t 0") # Shut down the computer
+        raise DialogFinished # Trigger the custom exception
+
+    elif response == win32con.IDNO: # Users clicks 'No'
+        print("Ignoring the alert.")
+        print("Sending email...")
+        Emailer(email) # Send the email
+        raise DialogFinished # Trigger the custom exception
+
+def runInBackground():
+    filePath = f'{os.getcwd()}/alarmsettings.xlsx' # get excel settings filepath
+
+    ws1 = dispatchExcelFile(filePath)  # Dispatch excel 
+    email = ws1.Range('E2').Value # Parent email address
+
+    # Use veto time if it's available. Otherwise, use set time
+    if ws1.Range('C2').Value == None:
+        timeLimit = ws1.Range('A2').Value
+    else: 
+        timeLimit = ws1.Range('C2').Value
+
+    # Format the time limit to hh:mm for the scheduler to work
+    formattedTimeLimit = formatDecimalTime(timeLimit)
+
+    # Schedule the displayDialog function to run at the specified time
+    schedule.every().day.at(formattedTimeLimit).do(displayDialog, email)
+
+    # Keep the program running
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+# Run program indefinitely until the dialog box pops up. If it does, the custom exception will execute and stop the program.
+try:
+    runInBackground()
+except DialogFinished:
+    print("Dialog finished, exiting script")
